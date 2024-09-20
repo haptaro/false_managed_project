c253d43b6b53c698078c8b9c1eb0429f8a3a706d
Kotaro Fukuo <kotaro56f@gmail.com>
2024-09-19 19:44:58 -0700

    add_model

--- example/models.py
+++ example/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField()
+    body = models.TextField()
+
+    class Meta():
+        managed = False
+        db_table = 'article'
